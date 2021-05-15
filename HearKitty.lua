6a96dfd7d247aba51eca67243d1502230d7fe9be
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-05-14 22:14:16 -0700

    Revert: reverted

--- HearKitty.lua
+++ HearKitty.lua
@@ -6,7 +6,7 @@
 -- Main non-UI code
 ------------------------------------------------------------
 
-HearKittyVersion = 1.0904
+HearKittyVersion = 1.0905
 
 -- Hear Kitty requires this version of VgerCore:
 local KittyVgerCoreVersionRequired = 1.09
