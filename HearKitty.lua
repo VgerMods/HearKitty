3fd4fe6bc8de58c76eec353a98515fbcfba0f98b
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2025-05-26 08:44:34 -0700

    Rev version to 1.11.6

--- HearKitty.lua
+++ HearKitty.lua
@@ -6,7 +6,7 @@
 -- Main non-UI code
 ------------------------------------------------------------
 
-HearKittyVersion = 1.1105
+HearKittyVersion = 1.1106
 
 -- Hear Kitty requires this version of VgerCore:
 local KittyVgerCoreVersionRequired = 1.17
